16e4da265412506651f5b7b0e39b16606d1dfae5
d-e-e-p <deep@tensorfield.ag>
2020-03-22 01:35:19 -0700

    updated with tracking fudge factors

--- tracker/tracker.cpp
+++ tracker/tracker.cpp
@@ -25,6 +25,9 @@
 #include <thread>
 #include <chrono>
 
+// determined experimentally by tuning
+float g_conversion_factor = 5250;
+float g_starting_position = 2.040;      // in m
 
 using namespace cv;
 using namespace std;
@@ -32,6 +35,22 @@ namespace fs = std::experimental::filesystem;
 
 Ptr<Stitcher> stitcher;
 
+ 
+/* 
+ *
+ * g_conversion_factor    5800    5500    5250    5200
+ *                                             
+ *               real     est     est      est    est
+ * img0001421    2.040   2.040   2.040    2.040   2.040
+ * img0002657    0.000   0.187   0.086   -0.007  -0.027
+ *  delta        2.040   1.853   1.954    2.047   2.067
+ *                                             
+ *                                             
+ * img0004457    0.000           0.098 	  0.006  -0.014
+ * img0009217    2.160           2.147	  2.152   2.153
+ *   delta       2.160           2.049	  2.146   2.167
+ *                         
+ */
 
 
 int setup_stitcher() {
@@ -39,26 +58,26 @@ int setup_stitcher() {
     double work_scale = 1, seam_scale = 1, compose_scale = 1;
     bool is_work_scale_set = false, is_seam_scale_set = false, is_compose_scale_set = false;
 
-    LOGLN("setting up stitching pipeline...");
-	stitcher = Stitcher::create(Stitcher::SCANS);
+    LOGLN("setting up stitching pipeline with g_conversion_factor = " << g_conversion_factor);
+        stitcher = Stitcher::create(Stitcher::SCANS);
 
     Ptr<Feature2D> finder;
     finder = ORB::create();
     //finder = xfeatures2d::SURF::create();
     //finder = xfeatures2d::SIFT::create();
-	stitcher->setFeaturesFinder(finder);
+        stitcher->setFeaturesFinder(finder);
 
     Ptr<FeaturesMatcher> matcher;
-	bool full_affine = false; bool try_use_gpu = true; float match_conf = 0.3f; int num_matches_thresh1 = 6 ;
+        bool full_affine = false; bool try_use_gpu = true; float match_conf = 0.3f; int num_matches_thresh1 = 6 ;
      matcher = makePtr<detail::AffineBestOf2NearestMatcher>( full_affine , try_use_gpu , match_conf , num_matches_thresh1);
-	// TODO: free the last matcher
-	stitcher->setFeaturesMatcher(matcher);
+        // TODO: free the last matcher
+        stitcher->setFeaturesMatcher(matcher);
 
-	float conf_thresh = 0.1f; // threshold for two images are adjacent
-	Ptr<detail::BundleAdjusterBase> adjuster = stitcher->bundleAdjuster();
+        float conf_thresh = 0.1f; // threshold for two images are adjacent
+        Ptr<detail::BundleAdjusterBase> adjuster = stitcher->bundleAdjuster();
     adjuster->setConfThresh(conf_thresh);
 
-	return 0;
+        return 0;
 
 }
 
@@ -76,7 +95,7 @@ std::vector<fs::path> get_filenames( fs::path path ) {
             filenames.push_back( iter->path() ) ;
     }
 
-	sort(filenames.begin(), filenames.end());
+        sort(filenames.begin(), filenames.end());
     return filenames ;
 }
 
@@ -86,27 +105,31 @@ vector<ImageFeatures> features(2);
 
 pair<float,float> process_two_files(fs::path lastfile, fs::path nextfile) {
 
-	Ptr<Feature2D> finder = stitcher->featuresFinder();
+        Ptr<Feature2D> finder = stitcher->featuresFinder();
 
-	// assume old image is cached
-	if (image[0].empty()) {
-		image[0] = imread(lastfile.string());
-    	computeImageFeatures(finder, image[0], features[0]);
-	}
-	image[1] = imread(nextfile.string());
+        // assume old image is cached
+        if (image[0].empty()) {
+                image[0] = imread(lastfile.string());
+        computeImageFeatures(finder, image[0], features[0]);
+        }
+        image[1] = imread(nextfile.string());
     computeImageFeatures(finder, image[1], features[1]);
 
-	string basename = lastfile.stem().string();
-	LOG("Features in image " << basename << " = " << features[1].keypoints.size());
+        string basename = lastfile.stem().string();
+        if (features[1].keypoints.size() < 100) {
+                LOGLN(basename << " : low features in image = "  << features[1].keypoints.size());
+        }
 
-	Ptr<FeaturesMatcher> matcher = stitcher->featuresMatcher();
-	vector<MatchesInfo> pairwise_matches;
+        Ptr<FeaturesMatcher> matcher = stitcher->featuresMatcher();
+        vector<MatchesInfo> pairwise_matches;
    (*matcher)(features, pairwise_matches);
     matcher->collectGarbage();
-	LOGLN(" matching confidence = " << pairwise_matches[1].confidence);
+        if (pairwise_matches[1].confidence < 1.0) {
+                LOGLN(basename << " : low matching confidence = " << pairwise_matches[1].confidence);
+        }
 
     vector<CameraParams> cameras;
-	Ptr<Estimator> estimator = stitcher->estimator();
+        Ptr<Estimator> estimator = stitcher->estimator();
     if (!(*estimator)(features, pairwise_matches, cameras)) {
         cout << "Homography estimation failed.\n";
         return make_pair(0, 0);
@@ -119,24 +142,23 @@ pair<float,float> process_two_files(fs::path lastfile, fs::path nextfile) {
         //LOGLN("Initial camera intrinsics #" << indices[i]+1 << ":\nK:\n" << cameras[i].K() << "\nR:\n" << cameras[i].R);
     }
 
-	Ptr<detail::BundleAdjusterBase> adjuster = stitcher->bundleAdjuster();
+        Ptr<detail::BundleAdjusterBase> adjuster = stitcher->bundleAdjuster();
     if (!(*adjuster)(features, pairwise_matches, cameras)) {
         cout << "Camera parameters adjusting failed.\n";
         return make_pair(0, 0);
     }
 
-	//LOGLN("camera1 R: \n" << cameras[1].R << "\n");
+        //LOGLN("camera1 R: \n" << cameras[1].R << "\n");
 
-	float convertion_to_m = 0.0006;
-    float x = - cameras[1].R.at<float>(0,2) * convertion_to_m;
-    float y = - cameras[1].R.at<float>(1,2) * convertion_to_m;
+    float x =  cameras[1].R.at<float>(0,2) / g_conversion_factor;
+    float y =  cameras[1].R.at<float>(1,2) / g_conversion_factor;
 
-	pair<float,float> delta = make_pair(x,y);
+        pair<float,float> delta = make_pair(x,y);
 
 
-	//features[0] = features[1].clone();
-	swap(image[0],image[1]);
-	swap(features[0],features[1]);
+        //features[0] = features[1].clone();
+        swap(image[0],image[1]);
+        swap(features[0],features[1]);
 
 
     // Draw SIFT keypoints
@@ -148,9 +170,12 @@ pair<float,float> process_two_files(fs::path lastfile, fs::path nextfile) {
 
 int annotate_image(fs::path from, fs::path to, string text) {
 
-    string programName = "convert -pointsize 30 -fill yellow -font Inconsolata -draw \'text 10,50 \" " + text + " \" \'   " + from.string() + " " + to.string();
-    //LOGLN("running cmd: " << programName);
-    system( (const char *) programName.c_str());
+
+        string cmd = "magick " + from.string() + " -clahe 25x25%+128+2 -pointsize 30 -font Inconsolata -fill yellow -gravity East -draw \'translate 15,220 rotate 90 text 0,0 \" " + text + " \" \'   " + to.string() ;
+
+    //string cmd = "magick -pointsize 30 -fill yellow -font Inconsolata -draw \'text 10,50 \" " + text + " \" \'   " + from.string() + " " + to.string();
+    //LOGLN("running cmd: " << cmd);
+    system( (const char *) cmd.c_str());
 
 }
 
@@ -158,14 +183,14 @@ void save_image_position(fs::path jpgname, string bufstr) {
 
         fs::path trkname = jpgname;
 
-        // if raw directory is called "raw", then replace with "dng"
+        // if source directory is called "jpg", then replace with "jpg_tracking"
         if (trkname.parent_path().stem() == "jpg") {
-            string newpath = trkname.parent_path().parent_path().string() + "/trk/";
+            string newpath = trkname.parent_path().parent_path().string() + "/jpg_tracking/";
             fs::create_directories(newpath);
             trkname = newpath + trkname.stem().string() + ".jpg";
         }
 
-        LOGLN("name = " << jpgname << " -> " << trkname);
+        //LOGLN("name = " << jpgname << " -> " << trkname);
         annotate_image(jpgname,trkname, bufstr);
 }
 
@@ -182,7 +207,7 @@ int main(int argc, char** argv ) {
 
   // setup for loop
   fs::path lastfile = "";
-  pair<float,float> pos (0,0);
+  pair<float,float> pos (0,g_starting_position);
   char buf[BUFSIZ];
   vector<thread> threads;
 
@@ -195,22 +220,22 @@ int main(int argc, char** argv ) {
         lastfile = nextfile;
         continue;
     }
-    LOGLN("processing " << lastfile << " + " << nextfile);
+    //LOGLN("processing " << lastfile << " + " << nextfile);
     pair<float,float> delta = process_two_files(lastfile, nextfile);
-	pos.first  += delta.first;
-	pos.second += delta.second;
-	string basename = lastfile.stem().string().c_str();
-	sprintf (buf, "%s (%07.3f,%07.3f)m", &basename[0], pos.first, pos.second);
-	string bufstr(buf);
-	LOGLN(bufstr);
+        pos.first  += delta.first;
+        pos.second += delta.second;
+        string basename = lastfile.stem().string().c_str();
+        sprintf (buf, "%s (%07.3f,%07.3f)m", &basename[0], pos.first, pos.second);
+        string bufstr(buf);
+        LOGLN(bufstr);
 
-	threads.push_back(thread(save_image_position, lastfile, bufstr));
+        threads.push_back(thread(save_image_position, lastfile, bufstr));
 
     lastfile = nextfile;
   }
 
   for (auto& th : threads) {
-	th.join();
+        th.join();
   }
 
 
